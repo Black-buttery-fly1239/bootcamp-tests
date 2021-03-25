7a8a27bd8dd5097977242e7a0a756d7eb76c5431
nobu-hope <nobuhlelebz.com>
2021-03-25 17:21:35 +0200

    bootcamp-test changes

--- functions/fromWhere.js
+++ functions/fromWhere.js
@@ -1,10 +1,10 @@
 function fromWhere(regNo){
     switch(regNo){
-      case "CY":
+      case "CY 2965":
         return "Bellville";
-      case "CJ":
+      case "CJ 5643":
         return "Paarl";
-      case "CA":
+      case "CA 7654":
         return "Cape Town";
       default:
         return 'Some other place!';
--- test/fromWhere.tests.js
+++ test/fromWhere.tests.js
@@ -1,21 +1,21 @@
 describe('fromWhere' , function(){
     it('should return the town the car is from, if the reg number starts with CY, return Bellville ' , function(){
-        assert.equal(fromWhere("CY"), "Bellville");
+        assert.equal(fromWhere("CY 2965"), "Bellville");
 
     });
 
     it('should return the town the car is from, if the reg number starts with CJ, return Paarl ' , function(){
-        assert.equal(fromWhere("CJ"), "Paarl");
+        assert.equal(fromWhere("CJ 5643"), "Paarl");
 
     });
 
     it('should return the town the car is from, if the reg number starts with CA, return Paarl ' , function(){
-        assert.equal(fromWhere("CA"), "Cape Town");
+        assert.equal(fromWhere("CA 7654"), "Cape Town");
 
     });
 
     it('should return the town the car is from, if the reg number starts with default, return some other place! ' , function(){
-        assert.equal(fromWhere("default"), "Some other place!");
+        assert.equal(fromWhere("CY 890"), "Some other place!");
 
     });
 });
